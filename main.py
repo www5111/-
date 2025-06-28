0ace29ea5f389f57bd650224fd841404e292f8a7
wyc <1654194998@qq.com>
2025-06-28 10:49:36 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# 变量赋值
+name = "KUN"  # 字符串
+age = 25        # 整数
+height = 1.75   # 浮点数
+is_student = False  # 布尔值
+
+# 查看变量类型
+print(type(name))   # <class 'str'>
+print(type(age))    # <class 'int'>
+print(type(height)) # <class 'float'>
+print(type(is_student)) # <class 'bool'>
